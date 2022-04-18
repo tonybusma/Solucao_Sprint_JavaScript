6902376d76e85d3e7e1d4a3d6f68f243cecd6f7f
tonybusma <tonybusma@gmail.com>
2022-04-18 01:59:59 -0300

    Enviando soluçao do form

--- js/carousel.js
+++ js/carousel.js
@@ -4,8 +4,6 @@
 
 //Array storage class
 let carouselArr = [];
-const imgs = 0;
-const img = 0;
 
 //class Carousel
 class Carousel {
@@ -13,6 +11,12 @@ class Carousel {
     image;
     title;
     url;
+
+    constructor(image, title, url){
+        this.image = image;
+        this.title = title;
+        this.url = url;
+    }
 
     static Start(arr) {
         if (arr) {
@@ -30,6 +34,29 @@ class Carousel {
     }
 
     static Next() {
-
+        for(let i = 0; i < Carousel._size; i++){
+            carouselArr[i];
+        }
     }
-};
+}
+
+
+function AddImg(carouselArr){
+    let link = document.createElement("a");
+    link.href = Carousel.url;
+    
+    let img = document.createElement("img");
+    img.src = "img/" + Carousel.image;
+    img.title = Carousel.title;
+    
+    link.textContent = img;
+}
+
+let addImage = document.getElementById('carousel');
+let newImage = AddImg()
+
+
+carouselArr.push(new Carousel("imagem_1.jpg","Esta é a nova Ranger Ford 2022. Verifique novidades.","lancamento.html"));
+carouselArr.push(new Carousel("imagem_2.jpg","Ford a nossa história","#"));
+carouselArr.push(new Carousel("imagem_3.jpg","Nova Ford Bronco Sport 2022","lancamento.html"))
+console.log(carouselArr);
--- js/form.js
+++ js/form.js
@@ -1,8 +1,21 @@
-
 //class contato
 
 class contato {
-    
+    _nome;
+    _sobrenome;
+    _email;
+    _cpf;
+    _telefone;
+    _contato;
+
+    constructor(nome, sobrenome, email, cpf, telefone, contato){
+        this._nome = nome;
+        this._sobrenome = sobrenome;
+        this._email = email;
+        this._cpf = cpf;
+        this._telefone = telefone;
+        this._contato = contato;
+    }
 }
 
 function Post(form) {
@@ -23,5 +36,32 @@ function Enviar() {
     if (nome.value != "") {
         alert('Obrigado sr(a) ' + nome.value + ' os seus dados foram encaminhados com sucesso');
     }
-
 }
+
+
+const botao = document.getElementById('button');
+
+botao.addEventListener("click", function(event){
+    console.log(Enviar());
+})
+
+function Transicao(){
+    botao.style.background = "white";
+    botao.style.border = "#1351d8 solid 2px";
+    botao.style.width = "240px";
+    botao.style.height = "45px";
+    botao.style.fontSize = "20px";
+    botao.style.cursor = "pointer";
+    botao.style.color = "#1351d8";
+    botao.style.fontWeight = "bold";
+}
+
+botao.addEventListener("mouseover", Transicao);
+botao.addEventListener("mouseout", function(){
+    botao.style.background = "#1351d8";
+    botao.style.border = "none";
+    botao.style.width = "200px";
+    botao.style.height = "40px";
+    botao.style.fontSize = "13px"
+    botao.style.color = "white";
+});
